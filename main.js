86e009d9131654500f6b607f54ddc828e8f3fc68
JoshESandoval <joshua.45.sandoval@gmail.com>
2022-01-11 06:57:48 -0800

    added support for phones and smaller resolutions

--- main.js
+++ main.js
@@ -1,44 +1,38 @@
-let peoples = document.getElementById("people");
-peoples.innerHTML = "peoples";
+//Tip Calculator meant to seperate money based on hours worked (not for servers)
 
+//Initializes page and 
+let peoples = document.getElementById("people");
+console.log(peoples.childNodes.length);
 let tips = 0;
 
 let people = new Array();
 people.push(new Object());
 people.pop();
 
-class Person{
-	constructor(){
+let rounded = false;
+
+class Person {
+	constructor() {
 		this.name = '';
 		this.hours = 0;
 		this.tips = 0;
 		this.id;
 		this.label = "";
 	}
-	SetPerson(name, hours, tips, id){
+
+	SetPerson(name, hours, tips, id) {
 		this.name = name;
 		this.hours = hours;
 		this.tips = tips;
 		this.id = id;
-		this.label = "<input type = 'button' value = " + name.toString() + " onclick = Person.SetHours("+ this.id + ") />";	
+		this.label = "<p><input class='aButton' type = 'button' value = " + name.toString() + " onclick = Person.SetHours(" + this.id + ") />";
 	}
 
+	
 	static SetHours(num){
 		people[num].hours = prompt("Hours of" + Person.GetName(people[num]).toString());
 		console.log(people[num].name);
 		Update();
-	}
-
-	static GetLabel(Person_){
-		return Person_.label;
-	}
-
-	static GetHours(Person_){
-		return Person_.hours;
-	}
-
-	static GetTips(Person_){
-		return Person_.tips;
 	}
 
 	static GetName(Person_){
@@ -46,12 +40,9 @@ class Person{
 	}
 
 	static setLabel(Person_){
-		Person_.label = "<input type = 'button' value = " + Person.GetName(Person_) + " onclick = 'Person.SetHours()' />";
+		Person_.label = "<p><input class='aButton' type = 'button' value = " + Person.GetName(Person_) + " onclick = 'Person.SetHours()' />";
 	}
 	
-	static SetTip(Person_, num){
-		Person_.tips = num;
-	}
 }
 
 
@@ -62,21 +53,29 @@ function addPerson(name, hours, tips){
 	Update();
 }
 
+
+
 function addPersonButton(){
 	let aPerson = new Person();
 	let name = prompt("name");
 	let hours = prompt("hours");
+
 	aPerson.SetPerson(name, hours, 0, people.length);
+	let temp = document.createElement('p');
+	temp.className = 'aPerson';
+	temp.id = name;
+	temp.innerHTML = aPerson.label + "--- Hours: " + aPerson.hours + " -- Tips: " + aPerson.tips+ "</p>" ;
+
+	peoples.appendChild(temp);
 	people.push(aPerson);
-	Update();
 }
 
 function Update(){
-	peoples.innerHTML = '';
-	for(x of people){
-		peoples.innerHTML = peoples.innerHTML + Person.GetLabel(x) + "--- Hours: " + Person.GetHours(x) + " -- Tips: " + makeToInt(Person.GetTips(x)) + "<br>";
+	for (let x = 0; x < people.length; x++){
+		let temp = people[x];
+		peoples.childNodes[x + 1].innerHTML =  temp.label +  "--- Hours: " + temp.hours + " -- Tips: " + makeToInt(temp.tips) + "</p>";
 	}
-	console.log(peoples.innerHTML.toString());
+	console.log(peoples.childNodes.length);
 }
 
 function setTips(){
@@ -89,14 +88,17 @@ function CalculateTips(){
 	let ratio = 0;
 	let check = 0;
 	if(people.length != 0){
-		for(x of people){
-			totalHours = parseFloat(totalHours, 10) + parseFloat( Person.GetHours(x) , 10);
+		for (let x = 0; x < people.length; x++){
+			let temp = people[x];
+			console.log(temp.hours);
+			totalHours = parseFloat(totalHours, 10) + parseFloat( temp.hours , 10);
 		}
 		ratio = tips / totalHours;
 		console.log(totalHours, ratio);
-		for(let i = 0; i < people.length; i++){
-			check = (+check) + (+Person.GetHours(people[i])) * (+ratio);
-			Person.SetTip(people[i], (+Person.GetHours(people[i])) * (+ratio))
+		for (let i = 0; i < people.length; i++){
+			let temp = people[i];
+			check = (check) + (temp.hours * (ratio));
+			temp.tips = temp.hours * (ratio);
 		}		
 		Update();
 		let aCheck = document.getElementById("check");
@@ -104,7 +106,17 @@ function CalculateTips(){
 	}
 }
 
-function makeToInt(y){
-	let ret = y.toFixed(0);
-	return ret;
+function swapFloat() {
+	rounded = !rounded;
+	CalculateTips();
+}
+
+function makeToInt(y) {
+	if (rounded) {
+		let ret = y.toFixed(0);
+		return ret;
+	}
+	else {
+		return y;
+    }
 }
